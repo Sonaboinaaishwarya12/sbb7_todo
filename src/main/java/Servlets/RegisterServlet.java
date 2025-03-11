b88566973ec5197b03834858c416c4f2af63b977
Sonaboinaaishwarya12 <sonaboinaaishwarya@gmail.com>
2025-03-11 10:57:11 +0530

    sub2_todo project added to local repo

---
+++ src/main/java/Servlets/RegisterServlet.java
@@ -0,0 +1,10 @@
+package Servlets;
+
+public class RegisterServlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Servlets/TaskCompleteSevlet.java
@@ -0,0 +1,10 @@
+package Servlets;
+
+class TaskCompleteSevlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
